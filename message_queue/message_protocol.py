845db5a9276f079359c7fd3709a17ad83e1f9e3c
Bohdan Solianyk <bohdan_solianyk@epam.com>
2023-02-07 13:52:29 +0200

    refactor: add docstring

--- message_queue/message_protocol.py
+++ message_queue/message_protocol.py
@@ -2,6 +2,16 @@ from asyncio import StreamReader, StreamWriter
 
 
 async def read_message(stream: StreamReader) -> bytes:
+    """
+    On first connect, a client must send a message
+    containing the channel to subscribe to.
+    Thereafter, for the file of the connection,
+    a client sends a message to a channel
+    by first sending a message containing the destination channel name
+    followed by a message containing the data.
+    :param stream:
+    :return:
+    """
     size_bytes = await stream.readexactly(4)  # get the first 4 bytes (size prefix)
     size = int.from_bytes(size_bytes, byteorder="big")  # convert bytes into an integer
     data = await stream.readexactly(size)  # read payload size from stream
